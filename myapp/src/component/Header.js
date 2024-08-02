f7199819ae6ab52e27b9616e1024e7bdce259fe9
Ekam8684 <ekamdeep8684@gmail.com>
2024-08-02 00:30:51 -0400

    Add New Pages

--- myapp/src/component/Header.js
+++ myapp/src/component/Header.js
@@ -4,52 +4,63 @@ import { FaSearch } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
 import { FaLocationDot } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <>
-      <header className=' h-24 shadow-sm'>
-        <div className='h-full container mx-auto flex items-center justify-between px-32'>
+      <header className=' bg-white h-24 shadow-sm'>
+        <div className='h-full container mx-auto flex items-center justify-between px-16'>
           <div className=''>
-            <Logo w={100} h={80} />
-          </div>
-
-
-
-          <div className='  flex items-center justify-between border rounded-full focus-within:shadow '>
-            <div className=' h-14 px-3 outline-none flex items-center justify-center bg-black rounded-l-full text-white' >
-              <FaSearch className='h-6 w-6' />
-            </div>
-            <input className=' mx-1 px-3 outline-none ' type='text' placeholder='Search Stock ' />
+            <Link to={"/"}>
+              <Logo w={100} h={80} />
+            </Link>
           </div>
           <div className=' flex items-center justify-between '>
-            <div type="button" className=' flex items-center justify-between h-14 mx-2 w-45  border rounded-full focus-within:shadow '>
+            <div className='  flex items-center justify-between border rounded-full focus-within:shadow '>
               <div className=' h-14 px-3 outline-none flex items-center justify-center bg-black rounded-l-full text-white' >
-                <FaCartShopping className='h-6 w-6' />
+                <FaSearch className='h-6 w-6' />
               </div>
-              <div className='mx-3'>
-                <h2>Cart</h2>
-              </div>
+              <input className=' mx-1 px-3 outline-none ' type='text' placeholder='Search Stock ' />
             </div>
-
-            <div type="button" className=' flex items-center justify-between h-14 mx-2 w-45  border rounded-full focus-within:shadow '>
+            <Link to={"/cart"}>
+              <div type="button" className='hover:bg-gray-100 flex items-center justify-between h-14 mx-2 w-45  border rounded-full focus-within:shadow '>
+                <div className=' relative h-14 px-3 outline-none flex items-center justify-center bg-black rounded-l-full text-white' >
+                  <span> <FaCartShopping className='h-6 w-6' /></span>
+                  <div className=' m-1 absolute top-1 right-1 bg-white rounded-full flex justify-center items-center text-black w-5 h-5 '>
+                    <p className='flex justify-center items-center '>0</p>
+                  </div>
+                </div>
+                <div className='mx-3'>
+                  <h2>Cart</h2>
+                </div>
+              </div>
+            </Link>
+            <div type="button" className='hover:bg-gray-100 flex items-center justify-between h-14 mx-2 w-45  border rounded-full focus-within:shadow '>
               <div className=' h-14 px-3 outline-none flex items-center justify-center bg-black rounded-l-full text-white' >
+
                 <FaLocationDot className='h-6 w-6' />
+
               </div>
               <div className='mx-3'>
-                <h1>Address </h1>
-                <h2>Update Location</h2>
+                <p>Address</p>
+                <p>Update Location</p>
               </div>
             </div>
+            <Link to={"/login"}>
+              <div type="button" className=' hover:bg-gray-100   flex items-center justify-between h-14 mx-2 w-45  border rounded-full focus-within:shadow '>
+                <div className=' h-14 px-3 outline-none flex items-center justify-center bg-black rounded-l-full text-white' >
 
-            <div type="button" className=' flex items-center justify-between h-14 mx-2 w-45  border rounded-full focus-within:shadow '>
-              <div className=' h-14 px-3 outline-none flex items-center justify-center bg-black rounded-l-full text-white' >
-                <CgProfile className='h-7 w-7' />
+                  <CgProfile className='h-7 w-7' />
+                </div>
+                <div className='mx-3 '>
+                  <div className='flex items-center justify-items-start'>
+                    Hello, <p>Sign In</p>
+                  </div>
+                  <p>Account & Lists</p>
+                </div>
               </div>
-              <div className='mx-3 '>
-                <h1>Hello, Sign In</h1>
-                <h2>Account & Lists</h2>
-              </div>
-            </div>
+            </Link>
           </div>
         </div>
       </header >
--- myapp/src/routes/index.js
+++ myapp/src/routes/index.js
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import Home from '../pages/home'
+import Login from '../pages/login'
+import Cart from '../pages/cart'
+import Signup from '../pages/signup'
+import Forgotpassword from '../pages/forgotpassword'
+import Order from '../pages/order'
+import Profile from '../pages/profile'
 const router = createBrowserRouter([
     {
         path: "/",
@@ -8,7 +14,31 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "",
-                element:<Home/>
+                element: <Home />
+            },
+            {
+                path: "signup",
+                element: <Signup />
+            },
+            {
+                path: "login",
+                element: <Login />
+            },
+            {
+                path: "verify",
+                element: <Forgotpassword />
+            },
+            {
+                path: "profile",
+                element: <Profile />
+            },
+            {
+                path: "cart",
+                element: <Cart />
+            },
+            {
+                path: "order",
+                element: <Order />
             }
         ]
     }
